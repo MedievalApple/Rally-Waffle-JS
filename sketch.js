78e628d12e136686d90d2586aefe3de605c40f34
NateDCoder <blondkenathanr@jacksonpec.org>
2023-05-23 13:23:01 -0400

    Co-authored-by: skyfm347@gmail.com <skyfm347@gmail.com>

--- sketch.js
+++ sketch.js
@@ -1,46 +1,105 @@
 var player;
+var enemy;
 var blocks = [];
 var gridSize;
 var corners = [];
 //W = wall
 //S = Start
 //G = Grass
+//E = End
 var carImage;
 var map = [];
 var map2 = [];
+var map3 = [];
 function setup() {
-    createCanvas(800, 800);
-    gridSize = width / 8;
+    createCanvas(1600, 1600);
+    gridSize = width / 16;
     player = new Car();
+    enemy = new Car();
     map = [
-        ["W", "G", "G", "W", "W", "G", "G", "W"],
-        ["S", " ", " ", " ", " ", " ", " ", "G"],
-        [" ", "W", "W", " ", "W", "W", " ", " "],
-        [" ", "W", "W", " ", "W", "W", "W", " "],
-        [" ", "W", "W", " ", " ", " ", " ", " "],
-        [" ", "W", " ", " ", "G", "G", "G", " "],
-        [" ", " ", " ", "G", "G", "W", "G", " "],
-        ["W", "W", " ", "W", "G", "G", "G", " "]
+        ["S", "G", "G", "W", "W", "G", "W", "W", "W", "W", " ", " ", " ", " ", " ", "G"],
+        [" ", " ", " ", " ", " ", " ", " ", "G", "W", "G", " ", "W", "W", "G", " ", " "],
+        [" ", "W", "W", " ", "W", "W", " ", " ", " ", " ", " ", "W", "W", "G", "G", " "],
+        [" ", "W", "W", " ", "W", "W", "W", " ", "W", "W", "G", "G", "W", "G", "W", " "],
+        [" ", "W", "W", " ", " ", " ", " ", " ", "W", "W", "W", "G", "G", "G", "G", " "],
+        [" ", "W", " ", " ", "G", "G", "G", " ", " ", " ", " ", "G", "W", "W", "W", " "],
+        [" ", " ", " ", "G", "G", "W", "G", " ", " ", "W", " ", " ", " ", " ", " ", " "],
+        [" ", "G", " ", "W", "G", "G", "G", "G", " ", "W", " ", "G", "W", "W", "G", " "],
+        [" ", "W", " ", " ", " ", " ", " ", "W", "G", " ", " ", "G", "G", "W", " ", " "],
+        [" ", "G", " ", "G", "W", "G", " ", "G", "W", "G", " ", " ", " ", " ", " ", "G"],
+        [" ", " ", " ", " ", " ", " ", " ", " ", "G", "G", " ", "G", " ", "G", " ", "W"],
+        [" ", "W", "W", " ", "W", "G", "G", " ", " ", " ", " ", "G", " ", " ", " ", " "],
+        [" ", "W", "W", " ", "W", "W", "W", " ", "G", "G", "W", "G", " ", "G", "G", " "],
+        [" ", "W", " ", " ", " ", " ", " ", " ", "G", "W", "W", "G", " ", "W", "W", " "],
+        [" ", "W", " ", "G", "G", "W", "G", " ", " ", "G", "W", "G", " ", "G", "G", " "],
+        [" ", " ", " ", "W", "W", "W", "G", "W", " ", " ", " ", " ", " ", " ", " ", " "]
     ];
 
     map2 = [
-        ["W", " ", " ", " ", " ", " ", "W", "W"],
-        [" ", " ", "G", "W", "G", " ", " ", " "],
-        [" ", "G", "G", " ", "W", "W", "W", " "],
-        [" ", "W", "W", " ", "W", "W", "W", " "],
-        [" ", "W", "W", " ", " ", " ", " ", " "],
-        [" ", " ", "W", "G", " ", "W", "W", " "],
-        ["G", " ", " ", " ", " ", "W", "W", " "],
-        ["W", "W", "W", "G", " ", " ", " ", "S"]
+        ["W", " ", " ", " ", " ", " ", "G", "W", "G", " ", " ", " ", " ", " ", "G", "W"],
+        [" ", " ", "G", "W", "G", " ", " ", " ", " ", " ", "W", " ", "G", " ", " ", "G"],
+        [" ", "G", "G", "G", "W", "W", "W", " ", "G", "G", "W", " ", "W", "W", " ", " "],
+        [" ", "W", "W", "G", "W", "W", "W", " ", "G", "W", "W", " ", "W", "W", "W", " "],
+        [" ", "W", "W", "G", " ", " ", " ", " ", "W", "W", "W", " ", " ", " ", " ", " "],
+        [" ", " ", "W", "G", " ", "W", "W", " ", "G", "W", "W", " ", "G", "W", "W", " "],
+        ["G", " ", " ", " ", " ", "W", "W", " ", "G", "G", "G", " ", "W", "W", "W", " "],
+        ["W", "W", "W", "G", " ", " ", " ", "S", "W", "W", "G", " ", " ", " ", " ", " "],
+        ["W", " ", " ", " ", " ", " ", "W", "W", "W", " ", " ", " ", " ", " ", "W", "G"],
+        [" ", " ", "G", " ", "G", " ", " ", " ", " ", " ", " ", "W", "G", " ", " ", " "],
+        [" ", "G", "G", " ", "G", "W", "W", " ", "G", "W", " ", "W", "W", "W", "W", " "],
+        [" ", "W", "W", " ", " ", "W", "G", " ", " ", "G", " ", " ", "W", "W", "W", " "],
+        [" ", "W", "W", "G", " ", " ", " ", "W", " ", "W", " ", " ", " ", " ", " ", " "],
+        [" ", "G", "W", "W", " ", "W", " ", " ", " ", " ", " ", "G", " ", "W", "W", " "],
+        [" ", " ", " ", "W", " ", "W", "W", " ", "G", " ", " ", " ", " ", "W", " ", " "],
+        ["W", "G", " ", " ", " ", "G", "G", " ", "W", "W", "W", "G", " ", " ", " ", "G"]
+    ];
+
+    map3 = [
+        ["W", "G", "G", "W", "G", " ", "W", "G", "G", "W", " ", "G", "W", "G", "G", "W"],
+        ["G", "S", " ", "G", " ", " ", "G", "W", "W", "G", " ", " ", "G", " ", " ", "G"],
+        [" ", " ", " ", " ", " ", " ", "G", "W", "W", "G", " ", " ", " ", " ", " ", " "],
+        ["W", "G", "G", "W", " ", " ", "W", "G", "G", "W", "G", " ", "W", "G", "G", "W"],
+        ["G", "G", "G", "G", " ", " ", "G", "G", "G", "G", " ", " ", "G", "W", "W", "G"],
+        ["G", "G", "G", "G", " ", " ", "G", "G", "G", "G", " ", " ", "G", "W", "W", "G"],
+        ["W", "G", "G", "W", " ", " ", "W", "G", "G", "W", " ", " ", "W", "G", "G", "W"],
+        [" ", " ", " ", " ", " ", " ", "G", "W", "W", "G", " ", " ", " ", " ", " ", "G"],
+        ["G", " ", " ", "G", " ", " ", "G", "W", "W", "G", " ", " ", " ", " ", " ", "G"],
+        ["W", "G", "G", "W", "G", " ", "W", "G", "G", "W", "G", "G", "W", " ", "G", "W"],
+        ["G", " ", " ", "G", " ", " ", " ", " ", " ", "G", "G", "G", "G", " ", " ", "G"],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", "G", "G", "G", "G", " ", " ", "G"],
+        ["W", "G", "G", "W", "G", "G", "W", "G", "G", "W", "G", "G", "W", "G", "G", "W"],
+        ["G", "G", "G", "G", "W", "W", "G", "G", "G", "G", "W", "W", "G", "W", "W", "G"],
+        ["G", "G", "G", "G", "W", "W", "G", "G", "G", "G", "W", "W", "G", "W", "W", "G"],
+        ["W", "G", "G", "W", "G", "G", "W", "G", "G", "W", "G", "G", "W", "G", "G", "W"]
+    ];
+
+    map4 = [
+        ["S", " ", " ", " ", " ", " ", "W", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        ["W", " ", "W", "W", "W", " ", "W", " ", "W", " ", "W", " ", "W", " ", "W", " "],
+        [" ", " ", " ", " ", "W", " ", "W", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        ["W", "G", "W", "W", "W", " ", "W", " ", "W", " ", "W", " ", "W", " ", "W", " "],
+        ["G", "G", "G", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        ["W", "W", "W", " ", "W", "W", "W", " ", "W", " ", "W", " ", "W", " ", "W", " "],
+        [" ", " ", " ", " ", " ", " ", "W", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        ["W", " ", "W", " ", "W", " ", "W", " ", "W", " ", "W", " ", "W", " ", "W", " "],
+        [" ", " ", " ", " ", "W", " ", "W", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        ["W", " ", "W", "W", "W", "W", "W", " ", "W", " ", "W", " ", "W", " ", "W", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        ["W", " ", "W", " ", "W", " ", "W", " ", "W", " ", "W", " ", "W", "W", "W", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "W", " "],
+        ["W", " ", "W", " ", "w", " ", "W", " ", "W", " ", "W", " ", "W", " ", "w", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "W", " ", " ", " "],
+        ["W", " ", "W", " ", "W", " ", "W", " ", "W", " ", "W", " ", "W", " ", "W", "E"]
     ];
     carImage = loadImage("./assets/player.png")
 }
 
 function draw() {
     background(255);
-    showMap(map);
+    showMap(map2);
     player.move();
     player.show();
+    enemy.show();
 }
 function showMap(theMap) {
     for (let i = 0; i < theMap.length; i++) {
@@ -58,7 +117,7 @@ function showMap(theMap) {
             }
         }
     }
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < 16; i++) {
         stroke(51);
         strokeWeight(3);
         line(i * gridSize, 0, i * gridSize, height);
@@ -70,7 +129,7 @@ class Car {
         this.pos = createVector(width / 2 - gridSize / 2, height / 2 - gridSize / 2);
         this.carAngle = 0;
         this.w = 32 * 2 - 10;
-        this.h = 32 * 2 - 14;
+        this.h = 32 * 2 - 12;
         this.vel = createVector(0, 0);
     }
     show() {
@@ -79,27 +138,27 @@ class Car {
         rotate(this.carAngle);
         noStroke();
         fill(255);
-        image(carImage, -this.w / 2-5, -this.h / 2, 64, 64);
+        image(carImage, -this.w / 2 - 5, -this.h / 2 - 6, 64, 64);
         pop();
-        stroke(255, 0, 0);
-        corners[0] = createVector(-this.w / 2, -this.h / 2).rotate(this.carAngle);
-        corners[0].add(this.pos);
+        // stroke(255, 0, 0);
+        // corners[0] = createVector(-this.w / 2, -this.h / 2).rotate(this.carAngle);
+        // corners[0].add(this.pos);
 
-        corners[1] = createVector(this.w / 2, -this.h / 2).rotate(this.carAngle);
-        corners[1].add(this.pos);
+        // corners[1] = createVector(this.w / 2, -this.h / 2).rotate(this.carAngle);
+        // corners[1].add(this.pos);
 
-        corners[2] = createVector(-this.w / 2, this.h / 2).rotate(this.carAngle);
-        corners[2].add(this.pos);
+        // corners[2] = createVector(-this.w / 2, this.h / 2).rotate(this.carAngle);
+        // corners[2].add(this.pos);
 
-        corners[3] = createVector(this.w / 2, this.h / 2).rotate(this.carAngle);
-        corners[3].add(this.pos);
+        // corners[3] = createVector(this.w / 2, this.h / 2).rotate(this.carAngle);
+        // corners[3].add(this.pos);
 
-        strokeWeight(4);
-        stroke(255, 0, 0);
-        point(corners[0].x, corners[0].y);
-        point(corners[1].x, corners[1].y)
-        point(corners[2].x, corners[2].y)
-        point(corners[3].x, corners[3].y)
+        // strokeWeight(4);
+        // stroke(255, 0, 0);
+        // point(corners[0].x, corners[0].y);
+        // point(corners[1].x, corners[1].y)
+        // point(corners[2].x, corners[2].y)
+        // point(corners[3].x, corners[3].y)
     }
     move() {
         if (keyIsDown(87)) { // w key
@@ -144,13 +203,75 @@ class Car {
         corners[3] = createVector(this.w / 2, this.h / 2).rotate(this.carAngle);
         corners[3].add(this.pos);
         corners[3].mult(1 / gridSize);
-        if (map[floor(corners[0].y)][floor(corners[0].x)] == "W") {
-            
+        // let v1 = (corners[0].copy().sub(corners[2])).mult(gridSize);
+        // let v2 = (corners[3].copy().sub(corners[2])).mult(gridSize);
+        // line(corners[2].x * gridSize, corners[2].y * gridSize, v1.x + corners[2].x * gridSize, v1.y + corners[2].y * gridSize);
+        // line(corners[2].x * gridSize, corners[2].y * gridSize, v2.x + corners[2].x * gridSize, v2.y + corners[2].y * gridSize);
+        // let angle1 = atan(v1.y / v1.x);
+        // let angle2 = atan(v2.y / v2.x);
+        // text(floor(angle1), this.pos.x - 10, this.pos.y);
+        // text(floor(angle2), this.pos.x + 10, this.pos.y);
+        if (map2[floor(corners[0].y)][floor(corners[0].x)] == "W") {
+            let v1 = (corners[1].copy().sub(corners[0])).mult(gridSize);
+            let angle1 = atan(v1.y / v1.x);
+            let v2 = (corners[2].copy().sub(corners[0])).mult(gridSize);
+            let angle2 = atan(v2.y / v2.x);
+            if (angle2 < 0) {
+                angle2 += PI;
+            }
+            if (angle1 < angle2) {
+                this.carAngle += PI / 120;
+            } else {
+                this.carAngle -= PI / 120;
+            }
             return true;
         }
-        if (map[floor(corners[1].y)][floor(corners[1].x)] == "W" ||
-            map[floor(corners[2].y)][floor(corners[2].x)] == "W" ||
-            map[floor(corners[3].y)][floor(corners[3].x)] == "W") {
+        if (map2[floor(corners[1].y)][floor(corners[1].x)] == "W") {
+            let v1 = (corners[0].copy().sub(corners[1])).mult(gridSize);
+            let v2 = (corners[3].copy().sub(corners[1])).mult(gridSize);
+            let angle1 = atan(v1.y / v1.x);
+            let angle2 = atan(v2.y / v2.x);
+            if (angle2 < 0) {
+                angle2 += PI;
+            }
+            if (angle1 < angle2) {
+                this.carAngle -= PI / 120;
+            } else {
+                this.carAngle += PI / 120;
+            }
+            return true;
+        }
+        if (map2[floor(corners[2].y)][floor(corners[2].x)] == "W") {
+            let v1 = (corners[0].copy().sub(corners[2])).mult(gridSize);
+            let v2 = (corners[3].copy().sub(corners[2])).mult(gridSize);
+            let angle1 = atan(v1.y / v1.x);
+            let angle2 = atan(v2.y / v2.x);
+            if (angle2 < 0) {
+                angle2 += PI;
+            }
+            if (angle1 < angle2) {
+                this.carAngle += PI / 120;
+            } else {
+                this.carAngle -= PI / 120;
+            }
+            return true;
+        }
+        if (map2[floor(corners[3].y)][floor(corners[3].x)] == "W") {
+            let v1 = (corners[1].copy().sub(corners[3])).mult(gridSize);
+            let v2 = (corners[2].copy().sub(corners[3])).mult(gridSize);
+            let angle1 = atan(v1.y / v1.x);
+            let angle2 = atan(v2.y / v2.x);
+            if (angle2 < 0) {
+                angle2 += PI;
+            }
+            if (angle1 < 0) {
+                angle1 += PI;
+            }
+            if (angle1 < angle2) {
+                this.carAngle -= PI / 120;
+            } else {
+                this.carAngle += PI / 120;
+            }
             return true;
         }
         return false;
