d8224de653c9da01040093e0587260eed9fb7c59
Medieval Apple <evergaming2500@gmail.com>
2023-05-30 21:18:32 -0400

    Added Score Upload Confirm

--- start.js
+++ start.js
@@ -1,7 +1,9 @@
 function startGame() {
     const username = document.getElementById("nameInput").value;
-    if(!username == "" || !username == " "){
+    console.log(username.length);
+    if(!username == "" && !username == " " && username.length <= 3){
         localStorage.setItem("username", username);
+        localStorage.setItem("uploadScores", document.getElementById("uploadCheck").checked);
         window.location.replace("rally.html");
     }
 }
