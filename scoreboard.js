badf98d4d7cea1de687f926c1ec5a1f1f8c8581f
NateDCoder <blondkenathanr@jacksonpec.org>
2023-06-05 13:05:06 -0400

    sorting for scores

--- scoreboard.js
+++ scoreboard.js
@@ -1,11 +1,23 @@
 var Counter = 1;
-
+var listOfScores = [];
 function addScore(playerName, Score) {
-    if (Counter <= 20) {
+    listOfScores.push({ "name": playerName, "score": Score });
+    sortScores(listOfScores);
+    var existingScores = document.getElementsByClassName("scoreItem");
+    for(let i = existingScores.length-1; i>=0; i--) {
+        existingScores[i].remove();
+    }
+    for(let counter = 0; counter < listOfScores.length&&counter<20; counter++) {
         let score = document.createElement("p");
-        score.innerText = Counter + " | " + playerName + ": " + Score;
+        score.innerText = (counter+1) + " | " + listOfScores[counter].name + ": " + listOfScores[counter].score;
         score.className = "scoreItem";
         document.getElementById("score").append(score);
-        Counter++;
     }
+}
+function sortScores(scores) {
+    scores.sort((a, b) => {
+        const time1 = a.score.split(":").join('')
+        const time2 = b.score.split(":").join('')
+        return parseInt(time1) - parseInt(time2)
+    });
 }
